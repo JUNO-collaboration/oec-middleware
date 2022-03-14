ca485b71d8ace7ef942d85123e899a8cadedcc95
yangyixiang <yangyixiang@ihep.ac.cn>
2022-03-14 09:40:08 +0800

    Modify the structure of oecRecEvt

--- CppSniper4LOEC/src/LOECOutputSvc.cc
+++ CppSniper4LOEC/src/LOECOutputSvc.cc
@@ -56,14 +56,30 @@ bool LOECOutputSvc::putQT(junoread::Event& onlineEvt){
     }
     auto& calibCol = calibHeader->event()->calibPMTCol();
     
+    //###################For test#######
+    int _calibNum = 0;
+    int _chargeExtreme = 0;
+    int _chargeNum = 0;
+    int _count = 0;
+
+    //###################For test#######
+
     static const int headerSize = 9;
     int size = headerSize;
     uint32_t* _cache = (uint32_t*)m_cache;
     for ( auto calib : calibCol ) {
+        _calibNum++;                      //DM: for test
+        if(_chargeNum > _chargeExtreme)   //DM: for test
+            _chargeExtreme = _chargeNum;  //DM: for test
+        if(_chargeNum > 20)               //DM: for test
+            _count++;                     //DM: for test  
+        _chargeNum = 0;                   //DM: for test
+
         int pmtId = calib->pmtId();
         auto& charge = calib->charge();
         auto& time = calib->time();
         for ( unsigned int i = 0; i < calib->size(); ++i ) {
+            _chargeNum++;//DM: for test
             _cache[size] = pmtId;
             float _charge = charge[i];
             float _time = time[i];
@@ -72,6 +88,10 @@ bool LOECOutputSvc::putQT(junoread::Event& onlineEvt){
             size += 3;
         }
     }
+    LogInfo<<"The number of Pmt "<<_calibNum<<std::endl;
+    LogInfo<<"The upper limi of the number of charge per pmt   "<<_chargeExtreme<<std::endl;
+    LogInfo<<"the number of Charges exceeding 20 "<<_count<<std::endl;
+
     //set the header
     int sizeInBytes = size*4;
     _cache[0] = 0xee1234ee;  //marker
@@ -91,6 +111,7 @@ bool LOECOutputSvc::putQT(junoread::Event& onlineEvt){
     uint32_t TQtag = *reinterpret_cast<uint32_t*>(tqInterval.first);
     assert((TQtag >> 16) == static_cast<uint32_t>(junoread::data_type::WAVE_CONSTRUCTED_TQ));
     assert((TQtag & 0x0000ffff) == 0x0000fffe);
+    LogInfo<<"sizeInBytes  of TQ "<<sizeInBytes<<std::endl;
     assert(sizeInBytes < 1000000);//daq提供的内存上限是1M
 
     void* recTQresult = reinterpret_cast<void*>(*reinterpret_cast<uint64_t*>(tqInterval.first + 4));
@@ -113,10 +134,11 @@ bool LOECOutputSvc::putVertex(junoread::Event& onlineEvt)
     auto oecEvent = (JM::OECEvent*)oecHeader->event("JM::OECEvent");
 
     //set result to DAQ Buffer
-    static const int headerSize = 5;
+    static const int headerSize = 0;//重建的顶点结果不要header
     int size = headerSize;
     uint32_t* _cache = (uint32_t*)m_cache;
     oec::OECRecEvt* _evt = (oec::OECRecEvt*)(_cache+size);
+    _evt->marker = 0x12345678;
     _evt->evtId = oecHeader->EventID();
     const auto& time = nav->TimeStamp();
     _evt->sec = time.GetSec();
@@ -127,13 +149,17 @@ bool LOECOutputSvc::putVertex(junoread::Event& onlineEvt)
     _evt->x = oecEvent->getVertexX();
     _evt->y = oecEvent->getVertexY();
     _evt->z = oecEvent->getVertexZ();
+    _evt->muinx = oecEvent->getMuInX();
+    _evt->muiny = oecEvent->getMuInY();
+    _evt->muinz = oecEvent->getMuInZ();
+    _evt->muid  = oecEvent->getMuID();
 
-    int sizeInBytes = size*4 +sizeof(oec::OECRecEvt);
-    _cache[0] = 0x12345678;  //marker
-    _cache[1] = headerSize*4;  //HeaderSize
-    _cache[2] = 0;  //qt vertex size in bytes
-    _cache[3] = sizeof(oec::OECRecEvt);  //waveform vertex size in bytes
-    _cache[4] = m_buf->l1id;  //l1id
+    int sizeInBytes = size*4 +sizeof(oec::OECRecEvt);//把 header拿到结构体部
+    //_cache[0] = 0x12345678;  //marker
+    //_cache[1] = headerSize*4;  //HeaderSize
+    //_cache[2] = 0;  //qt vertex size in bytes
+    //_cache[3] = sizeof(oec::OECRecEvt);  //waveform vertex size in bytes
+    //_cache[4] = m_buf->l1id;  //l1id
 
     std::pair<uint8_t*, uint8_t*> recResultInterval = onlineEvt.indexOf(junoread::Event::EventIndex::REC_RESULT);
     
@@ -141,7 +167,8 @@ bool LOECOutputSvc::putVertex(junoread::Event& onlineEvt)
     assert((recResultInterval.second - recResultInterval.first) == 12);
     assert((recTag >> 16) == static_cast<uint32_t>(junoread::data_type::REC_RESULT));
     assert((recTag & 0x0000ffff) == 0x0000ffff);
-    assert(sizeInBytes < 1000000);//daq提供的内存上限是1M
+    LogInfo<<"sizeInBytes  of vertex "<<sizeInBytes<<std::endl;
+    assert(sizeInBytes < 6000000);//daq提供的内存上限是6M
 
     void* recResult = reinterpret_cast<void*>(*reinterpret_cast<uint64_t*>(recResultInterval.first + 4));
     memcpy(recResult, _cache, sizeInBytes);
