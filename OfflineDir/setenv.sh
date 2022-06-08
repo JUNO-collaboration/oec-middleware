a59eab63a40e39d79b5068f3bf36407b83bf8ba8
yangyixiang <yangyixiang@ihep.ac.cn>
2022-06-08 17:00:35 +0800

    Change the LOEC config

--- OfflineDir/setenv.sh
+++ OfflineDir/setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export OFFLINE_DIR=`readlink -f ${BASH_SOURCE[0]} | xargs dirname`
-#echo "[INFO] use offline libraries: \"${OFFLINE_DIR}\""
+echo "[INFO] use offline libraries: \"${OFFLINE_DIR}\""
 export LD_LIBRARY_PATH=${OFFLINE_DIR}/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=${OFFLINE_DIR}/config:${OFFLINE_DIR}/python:$PYTHONPATH
 export SNIPER_INIT_FILE=${OFFLINE_DIR}/config/.init.json
